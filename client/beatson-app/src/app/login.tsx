843c09553dfc3d2b2db28e8e7208b459db549375
Aubrey Agub <2555752a@student.gla.ac.uk>
2023-11-22 14:27:45 +0000

    del next svgs and created page for login

---
+++ client/beatson-app/src/app/login.tsx
@@ -0,0 +1,15 @@
+export default function Login() {
+  return (
+    <main>
+      <div>
+        <title>
+          NMMN Data Portal - Login
+        </title>
+        <h1>
+          Login
+        </h1>
+
+      </div>
+    </main>
+  )
+}
