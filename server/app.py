ae4db12d73106afb7051d87c309c85d6f1c93a65
2387399m <2387399m@student.gla.ac.uk>
2023-12-27 04:00:04 +0000

    Updated example to include post method #COMMENT removed css, reformatted code on app.py, page.tsx

--- server/app.py
+++ server/app.py
@@ -1,16 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 
 CORS(app)
 
-@app.route("/api/home")
-def hello_world():
+@app.route("/api/home", methods=["GET"])
+def inital_hello_world():
+  if request.method == "GET":
+    return jsonify({
+      'message' : "Hello World!"
+    })
 
+@app.route("/api/home", methods=["POST"])
+def post_new_message():
+
+  if request.method == "POST":
+    print("post confirmed")
+    print("Post recived: " + str(request.json))
+    newMsg = request.json["message"]
   return jsonify({
-    'messgae' : "Hello World!"
-  })
-
+      'message' : newMsg + " (hello from server!)"
+    })
 if __name__ == "__main__":
   app.run(debug=True, port=8080)
