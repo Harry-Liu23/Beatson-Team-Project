6bc4a41329d5bd07ee4a6584deda3d8fd9b96579
2387399m <2387399m@student.gla.ac.uk>
2023-12-29 16:35:27 +0000

    Created components and login folder with respective loginPage and loginForm placeholders. Created requirements.txt for pip moduls in .../server/. Remove most content from index page on client side and change port number to 2020

--- client/beatson-app/src/app/page.tsx
+++ client/beatson-app/src/app/page.tsx
@@ -1,58 +1,23 @@
  "use client"
-import { Dispatch, SetStateAction, useEffect,useState } from "react"
-
-function sendMessageToAPI(newMessage: String, setMsg : Dispatch<SetStateAction<string>>){
-  console.log("attempting to send new message: " + newMessage)
-
-  const newMsgResult = fetch("http://127.0.0.1:8080/api/home", {
-    method : "POST",
-    mode : 'cors',
-    headers :{
-      'Content-Type' : 'application/json',
-      'Access-Control-Allow-Origin' : 'http://localhost:8080/'
-    },
-
-    body : JSON.stringify({ 'message' : newMessage})
-  }).then(
-    response => response.json())
-  .then(
-    data => {
-      setMsg(data.message)
-      console.log(data)
-    }
-  )
-}
+import { Dispatch, SetStateAction, use, useEffect,useState } from "react"
 
 export default function Home() {
   
   const [msg,setMsg] = useState("Loading...")
-  const [newMsg, setNewMsg] = useState("")
 
-  useEffect(() => {
-    fetch("http://127.0.0.1:8080/api/home", {
-      method: "GET"
-    })
-    .then(
-      (response) => response.json()
+  useEffect(() =>{
+    fetch("http://127.0.0.1:2020/").then(
+      (resp) => resp.json()
     ).then(
       (data) => {
         console.log(data)
         setMsg(data.message)
       }
     )
-    
   },[])
-  
   return (
     <main>
-      <h2>The current messge is : {msg}</h2>
-      <input name="newMessageBox" onChange={e => setNewMsg(e.target.value)}></input>
-      <button 
-      value="newMessage"
-      onClick={() => {
-        sendMessageToAPI(newMsg, setMsg)
-      }}>new message</button>
-      
+      <h2>{msg}</h2>
     </main>
   )
 }
--- server/server/main.py
+++ server/server/main.py
@@ -1,8 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from neo4j import GraphDatabase, basic_auth
 
 app=Flask(__name__)
 
+CORS(app)
 
 DATABASE_USERNAME = "neo4j"
 DATABASE_PASSWORD = "12345678"
@@ -15,14 +17,14 @@ session = driver.session()
 
 @app.route('/')
 def index():
-    return 'welcome to my webpage!'
+    return jsonify(
+        {
+            'message' : 'Welcome to my webapp!'
+        }
+    )
+
 #return render_template("something.html")
 #Above defaults to a html file in a folder called Templates, need to replace it with real file paths
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
 
 if __name__=="__main__":
