d97437429d650036553e40bc3cf383dc75d7990a
2387399m <2387399m@student.gla.ac.uk>
2023-12-30 00:01:27 +0000

    Changed  variable in tsconfig from es5 to es2015 to allow iterators to work. Started implementing login/logout session using flask_JWT_Extended. Added functional placeholder for LoginForm for testing reasons. Added server side login method for testing reason. Updated and relocated requirements.txt

--- client/beatson-app/src/app/components/LoginForm.tsx
+++ client/beatson-app/src/app/components/LoginForm.tsx
@@ -1,6 +1,56 @@
 "use client"
+
+import { FormEvent, useState} from "react"
+import internal from "stream"
+
 export default function LoginForm(){
+
+  const [isLoading,setLoading] = useState<boolean>(false)
+
+  async function onFormSubmit(event : FormEvent<HTMLFormElement>) {
+    
+    event.preventDefault()
+    setLoading(true) // disables the button so the user dosent submit multiple times
+
+    try{
+      const formData = new FormData(event.currentTarget)
+      const formJson = {
+        'username' : formData.get("uname"),
+        'password' : formData.get("pword")
+      }
+      const resp = await fetch("http://127.0.0.1:2020/login", {
+        method : 'POST',
+        mode : 'cors',
+        headers : {
+          'Content-Type' : 'application/json'
+        }
+      })
+
+      if(!resp.ok){
+        throw new Error("Post Failed :( ")
+      }
+      const data = await resp.json()
+      console.log(data)
+
+    }catch(error){
+      console.log(error)
+    }finally{
+      setLoading(false)
+    }
+
+  }
+
   return (
-    <h2>login form placeholder</h2>
+    <form onSubmit={e => onFormSubmit(e)}>
+      <label htmlFor="uname">Username</label>
+      <input type="text" name="uname">
+      </input>
+      <br></br>
+      <label htmlFor="uname">Password</label>
+      <input type="password" name="pword">
+      </input>
+      <br></br>
+      <button type="submit" disabled={isLoading}>submit</button>
+    </form>
   )
 }
