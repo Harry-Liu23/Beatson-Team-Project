c1575e4b3c38e8b14f7873da9027d8c5143fdbe0
2387399m <2387399m@student.gla.ac.uk>
2023-12-30 00:01:27 +0000

    Changed  variable in tsconfig from es5 to es2015 to allow iterators to work. Started implementing login/logout session using flask_JWT_Extended. Added functional placeholder for LoginForm for testing reasons. Added server side login method for testing reason. Updated and relocated requirements.txt

---
+++ client/beatson-app/src/app/services/user.services.ts
@@ -0,0 +1,5 @@
+import { FormEvent } from "react";
+
+export function FormToJson(formEvent : FormEvent<HTMLFormElement>){
+  
+}
--- server/server/main.py
+++ server/server/main.py
@@ -1,10 +1,16 @@
+import json
 from flask import Flask, request, jsonify
+from flask_jwt_extended import create_access_token,get_jwt_identity, JWTManager
 from flask_cors import CORS
 from neo4j import GraphDatabase, basic_auth
 
 app=Flask(__name__)
 
+# Change this on deployment
+app.config['JWT_SECRET_KEY'] = "please change this on deployment"
+
 CORS(app)
+jwt = JWTManager(app)
 
 DATABASE_USERNAME = "neo4j"
 DATABASE_PASSWORD = "12345678"
@@ -23,9 +29,25 @@ def index():
         }
     )
 
+@app.route("/login", methods=['POST','GET'])
+def login():
+
+    if request.method == 'GET':
+        print("Login GET")
+        return jsonify(msg = "get OK")
+    
+    if request.method == 'POST':
+        print("Login POST")
+        return jsonify(msg = "post OK")
+    
+    print("Unknown request recived: " + request.method)
+    return jsonify(msg = 'Unknow request :(')
+
+
+
 #return render_template("something.html")
 #Above defaults to a html file in a folder called Templates, need to replace it with real file paths
 
 
 if __name__=="__main__":
-    app.run(port=2020,host="127.0.0.1",debug=True)
+    app.run(port=2020,host="localhost",debug=True)
