a9dc8a0dd6a6ae04c2345e3f68580dc4ab7d7ff1
Haoran Liu <2785456L@student.gla.ac.uk>
2024-01-02 21:49:31 +0000

    Finished debugging, all CRUD methods work, a few setup updated

--- server/Infrastructure/entity/constraint/sample.py
+++ server/Infrastructure/entity/constraint/sample.py
@@ -1,33 +1,30 @@
 class sample:
-    def __init__(self,name,id,group,project):
+    def __init__(self, name, id, group, project):
         self.sample_name = name
         self.sample_ID = id
         self.sample_group = group
         self.sample_project = project
 
-    def set_sample_name(self,name):
-        self.sample = name
+    def set_sample_name(self, name):
+        self.sample_name = name
 
     def get_sample_name(self):
-        return self.sample
+        return self.sample_name
     
-    def set_sample_ID(self,id):
-        self.sample = id
+    def set_sample_ID(self, id):
+        self.sample_ID = id
 
     def get_sample_ID(self):
-        return self.id
+        return self.sample_ID
     
-    def set_sample_group(self,group):
-        self.group = group
+    def set_sample_group(self, group):
+        self.sample_group = group
 
     def get_sample_group(self):
-        return self.group
+        return self.sample_group
     
-    def set_sample_project(self,project):
-        self.project = project
+    def set_sample_project(self, project):
+        self.sample_project = project
 
     def get_sample_project(self):
-        return self.project
-    
-
-    
+        return self.sample_project
