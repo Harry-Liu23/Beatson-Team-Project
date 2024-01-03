8896d8d2237cb34aeec8633922d715c1efccbf58
Dan <2479176B@student.gla.ac.uk>
2024-01-03 17:39:22 +0000

    Added table for sample data entry Installed datagrid
    Gives users form canvas to enter sample details
    
    Added MUI DataGrid table widget to StudyForm.js temporarily
    
    StudyForm.js updated
    DataGrid table is now rendered at bottom of study form page
    
    Co-authored-by: Alyson Dick (student) <2084268D@student.gla.ac.uk>
    Co-authored-by: Mark Turnbull <2482252T@student.gla.ac.uk>

--- client/beatson-app/components/Study/StudyForm.js
+++ client/beatson-app/components/Study/StudyForm.js
@@ -5,6 +5,7 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
+import { DataGrid } from '@mui/x-data-grid';
 
 
 const StudyForm = () => {
@@ -14,6 +15,7 @@ const StudyForm = () => {
     const [organism, setOrganism] = useState("");
     const [description, setDescription] = useState("");
     const [sampleNumber, setSampleNumber] = useState("");
+    //let [rows, setRows] = useState([]);
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -24,13 +26,113 @@ const StudyForm = () => {
         console.log("Description: " + description);
         console.log("Number of Samples: " + sampleNumber);
     }
+
+    const columns = [
+
+    {
+        field: "id",
+        headerName: "Sample ID",
+        editable: true,
+        width: 100,
+    },
+    {
+        field: "sampleGroup",
+        headerName: "Sample Group",
+        editable: true,
+        width: 150,
+    },
+    {
+        field: "sampleProject",
+        headerName: "Sample Project",
+        editable: true,
+        width: 120,
+    },
+    {
+        field: "sampleDescription",
+        headerName: "Description",
+        editable: true,
+        width: 100,
+    },
+    {
+        field: "sampleOrganism",
+        headerName: "Organism",
+        editable: true,
+        width: 80,
+    },
+    {
+        field: "sampleTissue",
+        headerName: "Tissue",
+        editable: true,
+        width: 90,
+    },
+    {
+        field: "sampleSex",
+        headerName: "Sex",
+        editable: true,
+        width: 75,
+    },
+    {
+        field: "sampleCellLine",
+        headerName: "Cell Line",
+        editable: true,
+        width: 100,
+    },
+    {
+        field: "sampleBiomaterialProvider",
+        headerName: "Biomaterial Provider",
+        editable: true,
+        width: 150,
+    },
+    {
+        field: "sampleMouseModel",
+        headerName: "Mouse Model",
+        editable: true,
+        width: 100,
+    },
+    {
+        field: "sampleDatePrep",
+        headerName: "Date",
+        editable: true,
+        width: 60,
+        type: "date",
+    },
+    {
+        field: "sampleRepeat",
+        headerName: "Biological Repeat",
+        editable: true,
+        width: 150,
+    },
+    {
+        field: "sampleFastQ",
+        headerName: "FASTQ",
+        editable: true,
+        width: 150,
+    },
+    ]
+
+    const rows = [];
+
+    for(let i=0; i<4; i++) {
+        rows.push({id: i});
+    };
+  
+    // setRows((rows) => {
+    // for(let i=0; i<4; i++) {
+    //     rows.push({id: i});
+    // }});
     
+// const addRow = () =>{
+//     setRows
+// }
+
     return(
         <div>
             
-            {/* <Card sx={{ maxWidth: 600, justifyContent: "center" }} variant = "outlined" > */}
+            <Card variant = "outlined" >
             <Grid container 
-                direction = "column" 
+                rowGap={1}
+                columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+                direction = "column"
                 alignItems = "center"
                 justifyContent = "center"
                 spacing={2}
@@ -101,7 +203,29 @@ const StudyForm = () => {
 
             </Grid>
         
-        {/* </Card> */}
+        </Card>
+        
+
+            
+        <Grid container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        autoWidth
+        >
+            <Grid item sx = {{m: 2}}>
+                <Typography 
+                    variant="h4" 
+                    color="blue-gray" 
+                    align = "center">
+                    Sample Details
+                </Typography>
+            </Grid>
+            <Grid item>
+                {/* <Button onClick={addRow}>Add row</Button> */}
+                <DataGrid rows = {rows} columns = {columns}/>
+            </Grid>
+        </Grid>
         </div>
     );
 
