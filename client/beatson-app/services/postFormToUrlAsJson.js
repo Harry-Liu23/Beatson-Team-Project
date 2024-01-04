b6d3851e5a74e87c589d847983c448de172a9de1
2387399m <2387399m@student.gla.ac.uk>
2024-01-04 00:59:10 +0000

    Added post functionality for LoginForm. Added Services folder with function postFormToUrlAsJson.

---
+++ client/beatson-app/services/postFormToUrlAsJson.js
@@ -0,0 +1,28 @@
+export default async function postFormToUrlAsJson(event, url){
+
+  event.preventDefault();
+
+  /* [TODO] create a function that sends POST requests via url and body and returns response */
+  const formData = new FormData(event.target)
+  const userData = Object.fromEntries(formData.entries())
+
+  /* sends POST reqest to server with user data */
+
+  const reqest = await fetch(url, {
+          method : 'POST',
+          mode : 'cors',
+          body : JSON.stringify(userData),
+          headers : {
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin' : 'http://localhost:2020'
+          }
+      })
+
+      if( reqest.ok){
+        console.log('Request ok')
+        return reqest
+      }
+
+      return null
+
+}
