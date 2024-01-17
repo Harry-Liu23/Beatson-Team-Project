fc62d398fbf9bd3c4a0056349f6bf996e33b001f
Fraser Miller <frasermiller0@gmail.com>
2024-01-17 15:01:51 +0000

    Reintroduction of logic removed by mistake and files mistakenly deleted
    Included logic to add the top late or the git dir to the path varible as no way to include test modules without it has been found as of writing.
    Reintroduced __init__.py to test dir as it was deleted by mistake.
    
    Reintrodudec deleted code and file
    
    Allows for tests to be called in the same way you would a module. Allows for main application to refernce Infratructurer as a module.

--- server/run.py
+++ server/run.py
@@ -1,3 +1,10 @@
+import sys
+import os
+ 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, '..', '..'))
+sys.path.insert(0, project_root)
+
 from application import app
 from application import driver
 from application import session
--- server/setup.py
+++ server/setup.py
@@ -4,6 +4,6 @@ setup(
     name='beatson_app',
     version='1.0.0',
     packages = find_packages(),    
-    py_modules=['Infrastructure','server','test'],
+    py_modules=['Infrastructure','application','test'],
     include_package_data=True,
 )
