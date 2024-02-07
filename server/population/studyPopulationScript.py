98b3f91a40d3352840325e755cfbdeef720bef2b
Aubrey Agub <2555752a@student.gla.ac.uk>
2024-02-07 14:18:33 +0000

    Refactoring Pop Script
    Necessary to clean up data directory used for pop script.
    Refactors code to account for different types of nodes matching to a directory.
    No side effects of change.

--- server/population/studyPopulationScript.py
+++ server/population/studyPopulationScript.py
@@ -17,8 +17,8 @@ class PopulateStudies(unittest.TestCase):
     def tearDown(self): 
         pass
     
-    def populate(self, data_dir, files, method):
-        for f in files:
+    def populate(self, data_dir, node_files, method):
+        for f in node_files:
             file_path = os.path.join(data_dir, f)
             with open(file_path, 'r') as f:
                 data = json.load(f)
@@ -26,16 +26,11 @@ class PopulateStudies(unittest.TestCase):
                 self.assertEqual(response.status_code, 200)
 
     def test_populate_studies(self):
-        data_dir = os.path.join(current_dir, 'data')
-        data_dir_files = os.listdir(data_dir)
-        # keep fiiles that are studies
-        studies = [f for f  in data_dir_files if "study" in f.lower()]
-        experiments = [f for f in data_dir_files if "experiment" in f.lower()]
-        samples = [f for f in data_dir_files if "sample" in f.lower()]
-        # populate  database
-        self.populate(data_dir, studies, '/create_study')
-        self.populate(data_dir, experiments, '/create_experiment')
-        self.populate(data_dir, samples, '/create_sample')
+        node_types = ["study", "experiment", "sample"]
+        for node_type in node_types:
+            data_dir = os.path.join(current_dir, f'data/{node_type}')
+            node_files = os.listdir(data_dir)
+            self.populate(data_dir, node_files, f'/create_{node_type}')
         
         
 if __name__ == '__main__':
