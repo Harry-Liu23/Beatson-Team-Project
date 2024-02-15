6726d39b198f7e1bfdbb430ea66d179414cf1e39
Mark Turnbull <2482252t@student.gla.ac.uk>
2024-02-15 10:48:12 +0000

    BackendAPI file changed to conform to standards.
    Removes commented code, and helping console.logs to conform to style standards.
    In backendAPI.js commented out code was removed along with three console.logs used for developement.
    
    No other files changed

--- client/beatson-app/services/BackendAPI.js
+++ client/beatson-app/services/BackendAPI.js
@@ -1,37 +1,3 @@
-// export default async function postFormAsJSON(event, url){
-
-//   event.preventDefault();
-//   const formData = new FormData(event.target)
-//   const userData = Object.fromEntries(formData.entries())
-
-//   /* sends POST request to server with user data */
-
-//   const request = await fetch(url, {
-//     method : 'POST',
-//     mode : 'cors',
-//     body : JSON.stringify(userData),
-//     headers : {
-//         'Content-Type': 'application/json',
-//         'Access-Control-Allow-Origin' : 'http://localhost:2020'
-//         }
-//     })
-
-//   if(request.ok){
-//     console.log('Request ok')
-//     return request
-//   }
-
-//   return null
-
-// } // this function will be made redundant by below implementation
-// $.ajax({
-//   url: "/output",
-//   type: "POST",
-//   contentType: "application/json",
-//   data: JSON.stringify(postData),
-//   success: function(data){/* do something */}
-// });
-import ajax from 'axios';
 import $ from 'jquery';
 
 async function sendJsonToFlask(json, url) {
@@ -47,14 +13,8 @@ async function sendJsonToFlask(json, url) {
   })
     
     if (request.ok) {
-    console.log("ok")
     return request;
   }
-  else {
-    console.log("bad request")
-    console.log(request)
-  }
-
   return null;
 }
 
