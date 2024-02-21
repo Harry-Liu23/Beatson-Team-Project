b4809f57a9d5890be580ecb778bf97832928fc82
Dan <2479176B@student.gla.ac.uk>
2024-02-21 16:07:41 +0000

    generic function successfully returning json necessary to allow generic search as per requirements functionality added to handleSearchSubmit in SearchBar.js No side effects

--- client/beatson-app/components/dataDiscoverability/SearchBar.js
+++ client/beatson-app/components/dataDiscoverability/SearchBar.js
@@ -8,20 +8,35 @@ import {
     spacing,
   } from "@mui/material";
 
+//@app.route('/search_all_nodes/<search_string>',methods=['GET'])
+
 const SearchBar = () => {
-    const [searchQuery, setSearchQuery] = useState(null);
+    const [searchQuery, setSearchQuery] = useState("");
 
     const handleInputChange = (event) => {
         setSearchQuery(event.target.value);
     }
 
-    const handleSearchSubmit = (event) => {
+    const handleSearchSubmit = async (event) => {
         event.preventDefault();
-        if (searchQuery == null) {
+        if (searchQuery == "") {
             // do nothing there was no text field entered.
         }
         else {
             console.log(`Searching for : ${searchQuery}`);
+            try {
+                const response = await fetch("http://127.0.0.1:2020/search_all_nodes/" + searchQuery);
+                if (response.status !== 200) {
+                  throw new Error("Unable to fetch studies containing that text: ", data.message);
+                }
+                const data = await response.json();
+                console.log(data);
+                //setRows(data.study);
+                //console.log(rows);
+            }
+            catch (error) {
+                console.error("Unable to fetch data: ", error);
+            }
         }
     }
 
