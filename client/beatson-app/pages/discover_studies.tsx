f71da7d96f2e0220c570311553f559c062ed1304
Dan <2479176B@student.gla.ac.uk>
2024-02-21 14:58:07 +0000

    Added basic search bar to front end Necessary to facilitate searching .js file added for search component No side effects
    Co-authored-by: Mark Turnbull (student) <2482252T@student.gla.ac.uk>

--- client/beatson-app/pages/discover_studies.tsx
+++ client/beatson-app/pages/discover_studies.tsx
@@ -1,10 +1,12 @@
 import StudiesTable from "../components/dataDiscoverability/StudiesTable";
+import SearchBar from "../components/dataDiscoverability/SearchBar";
 
 /* website.url/discover_studies */
 
 export default function DiscoverStudies() {
   return (
     <div>
+      <SearchBar />
       <StudiesTable />
     </div>
   );
