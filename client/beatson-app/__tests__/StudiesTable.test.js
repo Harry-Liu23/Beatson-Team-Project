dc4d0a01cd4136300e429b26ff4370caff99d004
Abdulrahman <2502556s@student.gla.ac.uk>
2024-02-26 20:19:41 +0000

    Added Testing for StudiesTable and also Fixed an error form LoginForm
    Added the testing for the StudiesTable to ensure that it renders the study data correctly. And solved an error in LoginForm because of some Bakend Api.
    
    For the StudiesTable it ensure the all of the columnHeaders are there for example the Accession, Study Type, and etc.
    
    No side effects

---
+++ client/beatson-app/__tests__/StudiesTable.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import fetchMock from 'jest-fetch-mock';
+import StudiesTable from '../components/dataDiscoverability/StudiesTable'; 
+
+
+describe('StudiesTable', () => {
+
+    beforeEach(() => {
+        fetchMock.enableMocks(); 
+        fetchMock.resetMocks();
+    });
+
+    it('renders study data correctly ', async () => {
+    const mockStudyData = {
+        study: [
+            {
+                accession: 'E-MTAB-6265',
+                studyType: 'RNA-seq of coding RNA',
+                publication: 'Port,J., Muthalagu, N., Raja, M., Ceteci, F., Monteverde',
+                organism: 'Mus musculus',
+                description: 'The aim of the project is to determine the impact of NUAK1 depletion on the transcriptome of in situ colon tumours.'
+            },
+            {
+                accession: 'E-MTAB-6363',
+                studyType: 'High-throughput sequencing, RNA-seq of coding RNA',
+                publication: 'Undefined',
+                organism: 'Mus musculus',
+                description: 'Intestinal tumours from genetically engeenierd mouse modesl (GEMMs), of various gentopyes, were isolated at endpoint and RNA was isolated'
+            },
+            {
+                accession: 'E-MTAB-6620',
+                studyType: 'RNA-seq of coding RNA',
+                publication: 'Juliana B Candido, Jennifer P Morton, Peter Bailey',
+                organism: 'Mus musculus',
+                description: 'Recently, we defined four subtypes of pancreatic cancer that are associated with distinct histopathological characteristics and differential survival, namely, Squamous, Pancreatic Progenitor, Immunogenic, and ADEX (Bailey et al 2016, Nature).'
+            },
+        ]
+    };
+
+    fetchMock.mockResponseOnce(JSON.stringify(mockStudyData));
+
+    render(<StudiesTable />);
+
+    const columnHeaders = [
+        'Accession', 'Study Type', 'Publication', 'Organism','Description',
+    ];
+
+    await waitFor(() => {
+        columnHeaders.forEach(async header => {
+            expect(screen.findByText(header)).resolves.toBeInTheDocument();
+            });
+        });
+    });
+});
