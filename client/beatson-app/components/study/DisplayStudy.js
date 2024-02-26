cffd399f139667025f714c6f303e5dd324b49ad7
Aubrey Agub <2555752a@student.gla.ac.uk>
2024-02-26 15:26:58 +0000

    [WIP] Functioning Retrieval of  Experiments and Samples
    Necessary to get the data needed to be rendered on study page.
    For experiments, saving data directly into array. For samples, saving as javascript object with key as experiment_id.
    No side effects expected.
    
    Co-authored-by: Alyson Dick (student) <2084268D@student.gla.ac.uk>

--- client/beatson-app/components/study/DisplayStudy.js
+++ client/beatson-app/components/study/DisplayStudy.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
 
 const DisplayStudy = ({studyData}) => {
     const [experiments, setExperiments] = useState([]);
@@ -8,20 +9,28 @@ const DisplayStudy = ({studyData}) => {
     // const [samples, setSamples] = ({});
 
     //Gets list of all experiments
-    const getExpermentData = async () => {
+    const getExperimentData = async () => {
+      console.log("in getExperimentData function");
         try {
           const response = await fetch(`http://127.0.0.1:2020/get_all_experiments/${accession}`);
           if (response.status !== 200) {
             throw new Error("Unable to fetch studies: ", data.message);
           }
           const experimentData = await response.json();
-          console.log(experimentData);
-          setExperiments(experiments.push(experimentData));
+          setExperiments(experimentData.experiments);
+          // console.log(experimentData.experiments);
+          // setExperiments(experiments.push(experimentData));
         } catch (error) {
           console.error("Unable to fetch experiment data: ", error);
-        }
+        };
+      printExperiments(experiments);
     };
 
+    const printExperiments = (experiments) => {
+      experiments.forEach(element => {
+        console.log(element)
+      });
+    }
     //Gets sample data for one experiment
     const getSampleData = async (experiment_id) => {
         try {
@@ -30,9 +39,8 @@ const DisplayStudy = ({studyData}) => {
               throw new Error("Unable to fetch studies: ", data.message);
             }
             const samples = await response.json();
-            console.log(sampleData)
-            
-            setSampleData(sampleData.push(samples));
+            // console.log(sampleData)
+            setSampleData([...sampleData, samples]);
           } catch (error) {
             console.error("Unable to fetch sample data: ", error);
         }
@@ -41,20 +49,53 @@ const DisplayStudy = ({studyData}) => {
     //Gets all sample data for all experiments.
     const getAllSampleData = () => {
         const numberOfExperiments = sampleData.length;
+        console.log("in getAllSampleData");
         console.log(numberOfExperiments);
     }
 
     // Get experiments and samples data when DisplayStudy is first rendered
     useEffect(() => {
-        getExpermentData();
-        getAllSampleData();
+        getExperimentData();
+        let experimentWithSamples = {};
+        experiments.forEach(experiment => {
+          getSampleData(experiment.experiment_id)
+          experimentWithSamples[experiment.experiment_id] = sampleData;
+        });
+        // getAllSampleData();
+        console.log("experiments and samples", experimentWithSamples);
     }, []);
+
+
+    const [columns, setColumns] = useState([
+      {
+        field: "accession",
+        headerName: "Accession",
+        width: 200,
+      },
+      {
+        field: "description",
+        headerName: "Description",
+        width: 200,
+      },
+      {
+        field: "experiment_id",
+        headerName: "Experiment ID",
+        width: 200,
+      },
+    ]);
 
     return (
         <div>
             {studyData.accession}
-            {studyData.description}
+            <DataGrid
+              getRowId={(row) => row.experiment_id}
+              rows={experiments}
+              columns={columns}
+            />
+            {experiments.objects}
+            {/* {experimentWithSamples} */}
         </div>
+
 
     );
 };
