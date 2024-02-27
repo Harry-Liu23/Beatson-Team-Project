e196f4b14aabcf41dd72120aa282f055f92353da
Dan <2479176B@student.gla.ac.uk>
2024-02-27 15:33:37 +0000

    WIP Samples Table on discover studies page To allow sample discoverability as per requirements Adds SamplesTable.js component and logic to genericDao & generalService to accomodate retrieiving related study from a sample node Changes to SearchBar.js, SamplesTable.js, generalService.py and genericDao.py
    Co-authored-by: Mark Turnbull (student) <2482252T@student.gla.ac.uk>

---
+++ client/beatson-app/components/dataDiscoverability/SamplesTable.js
@@ -0,0 +1,137 @@
+import { useState, useEffect } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { 
+  Grid,
+  Card,
+ } from "@mui/material";
+
+ const SamplesTable = (prop) => {
+    // rows for samples table where each row corresponds to a sample
+    const [rows, setRows] = useState([]);
+    const [newChange, setNewChange] = useState(prop.change);
+  
+  
+    // define columns for samples table
+    const [columns, setColumns] = useState([
+      {
+        field: "sample_id",
+        headerName: "Sample ID",
+        width: 130,
+      },
+      {
+        field: "group",
+        headerName: "Group",
+        width: 100,
+      },
+      {
+        field: "project",
+        headerName: "Project",
+        width: 130,
+      },
+      {
+        field: "description",
+        headerName: "Description",
+        width: 200,
+      },
+      {
+        field: "organism",
+        headerName: "Organism",
+        width: 130,
+      },
+      {
+        field: "tissue",
+        headerName: "Tissue",
+        width: 100,
+      },
+      {
+        field: "sex",
+        headerName: "Sex",
+        width: 100,
+      },
+      {
+        field: "cell_line",
+        headerName: "Cell Line",
+        width: 100,
+      },
+      {
+        field: "related_study",
+        headerName: "Related Study",
+        width: 100,
+      },
+    ]);
+  
+    // if change to search has changed then call get studies data
+  
+    // retrieve all samples from Neo4J via Flask
+    const getSamplesData = () => {
+      setRows(prop.samples);
+    }
+
+    //add related study ID property to json sample objects
+    const addRelatedStudy = async () => {
+        const newSamples = prop.samples;
+        const returnSamples = [];
+
+        newSamples.forEach(async (sample) => {
+            let addSample = sample;
+            console.log(addSample);
+            let study_id = "";
+            try {
+                const response = await fetch("http://127.0.0.1:2020/get_study_from_experiment/" + sample.experiment_id);
+                if (response.status !== 200) {
+                  throw new Error("Unable to fetch studies containing that text: ", data.message);
+                }
+                study_id = await response.json();
+            }
+            catch (error) {
+                console.log("error" + error);
+            }
+
+            addSample["related_study"] = study_id;
+            returnSamples.push(addSample);
+        });
+        return returnSamples;
+    }
+  
+    // //get SamplesData when the samples table is first rendered
+    useEffect(() => {
+      getSamplesData();
+    }, []);
+  
+    if (newChange !== prop.change) {
+      try {
+        // setRows(prop.samples);
+        console.log(addRelatedStudy());
+        setRows(addRelatedStudy());
+        setNewChange(!newChange);
+      }
+      catch (error) {
+        console.log("No studies found " + error);
+      }
+    }
+  
+    return (
+      // display each sample as a row on mui DataGrid
+      <div>
+        <Card variant="outlined" sx={{ padding:6 }} > 
+          <Grid
+            container
+            rowGap={1}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            spacing={2}
+          >
+            <DataGrid
+              getRowId={(row) => row.sample_id}
+              rows={rows}
+              columns={columns}
+            />
+          </Grid>
+          </Card>
+      </div>
+    );
+  };
+  
+  export default SamplesTable;
+  
