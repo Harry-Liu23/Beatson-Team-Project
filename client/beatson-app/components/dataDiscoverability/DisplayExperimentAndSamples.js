8be18950fdb98b1b9f4adba80f421f992fe2faf3
Aubrey Agub <2555752a@student.gla.ac.uk>
2024-03-04 11:04:07 +0000

    Fixing Data Retrieval + Rendering Data on to Page
    Necessary as columns were not being saved which is needed to render samples data on datagrid, and also for users to see the data.
    Steps taken in DisplayExperimentandSamples: altered function for loading to make sure it waits for all data required including columns and made sure experiment and samples are always being saved first, and added MUI components to display the data itself, also removed debugging + unnecessary code.
    Steps taken in DisplayStudy: saved studyData, rendered study data using MUI components.
    Side effect expected: no more errors expected when using Study Discovery section of the web page.

--- client/beatson-app/components/dataDiscoverability/DisplayExperimentAndSamples.js
+++ client/beatson-app/components/dataDiscoverability/DisplayExperimentAndSamples.js
@@ -1,63 +1,68 @@
+import { Card, Grid, Typography } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
 
 const DisplayExperimentAndSamples = (props) => {
-    const experiment = props.experimentData;
-    const samples = props.samplesData;
-    const [columnNames, setColumnNames] = useState([]);
+  const experiment = props.experimentData;
+  const samples = props.samplesData;
+  const [columns, setColumns] = useState([]);
 
-    if (!samples && !experiment) {
-        return <div>Loading Experiments and Samples...</div>; 
+  useEffect(() => {
+    if (samples && samples.length > 0) {
+      console.log("First sample", samples[0]);
+      const samplesKeys = Object.keys(samples[0]);
+      console.log("Keys from first sample", samplesKeys);
+
+      const columnsFormat = samplesKeys.map((sampleKeys) => ({
+        field: sampleKeys,
+        headerName:
+          sampleKeys.charAt(0).toUpperCase() +
+          sampleKeys.slice(1).replace("_", " "),
+        width: 200,
+      }));
+      console.log("Formatted Columns", columnsFormat);
+      setColumns(columnsFormat);
     }
-    // const columnNames = Object.keys(samples[0]);
-    // console.log(samplesData);
-    
-    useEffect(() => {
-        if (samples && samples.length > 0) {
-            console.log("First sample", samples[0]);
-            const keys = Object.keys(samples[0]);
-            console.log("Keys from first sample", keys);
-            setColumnNames(Object.keys(samples[0]));
-        }
-    }, [samples]);
+  }, [samples]);
 
-    console.log("Experiment Data inside Display E+S", experiment);
-    console.log("Samples Data inside E+S", samples);
-    console.log("columnNames", columnNames);
-    // const columns = columnNames.map(columnName => ({
-    // field: columnName,
-    // headerName: columnName.charAt(0).toUpperCase + columnName.slice(1).replace('_', ' '),
-    // width: 200
-    // }));
+  console.log("Experiment Data inside Display E+S", experiment);
+  console.log("Samples Data inside E+S", samples);
+  console.log("columnNames", columns);
 
-    // const [columns, setColumns] = useState([
-    //   {
-    //     field: "accession",
-    //     headerName: "Accession",
-    //     width: 200,
-    //   },
-    //   {
-    //     field: "description",
-    //     headerName: "Description",
-    //     width: 200,
-    //   },
-    //   {
-    //     field: "experiment_id",
-    //     headerName: "Experiment ID",
-    //     width: 200,
-    //   },
-    // ]);
+  if (!samples || !experiment || columns.length < 1) {
+    return <div>Loading Experiments and Samples...</div>;
+  }
 
-    return (
-        <div>
-             {/* <DataGrid
-                getRowId={(row) => row.sample_id}
-                rows={samples}
-                // rows={[experiment]}
-                columns={columnNames}
-            /> */}
-        </div>
-    )
+  return (
+    <div>
+      <Card variant="outlined" sx={{ padding: 2 }}>
+        <Grid
+          container
+          rowGap={1}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          spacing={2}
+        >
+          <Grid item sx={{ mt: 1.5 }}>
+            <Typography variant="h4" color="blue-gray" align="center">
+              Experiment ID: {experiment.experiment_id}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            Experiment Details: {experiment.description}
+          </Grid>
+        </Grid>
+
+        <DataGrid
+          getRowId={(row) => row.sample_id}
+          rows={samples}
+          columns={columns}
+        />
+      </Card>
+    </div>
+  );
 };
 
 export default DisplayExperimentAndSamples;
--- client/beatson-app/components/dataDiscoverability/DisplayStudy.js
+++ client/beatson-app/components/dataDiscoverability/DisplayStudy.js
@@ -1,99 +1,133 @@
 import { useEffect, useState } from "react";
+import { Card, Grid, Typography } from "@mui/material";
 import DisplayExperimentAndSamples from "./DisplayExperimentAndSamples";
-import { DataGrid } from "@mui/x-data-grid";
 
-const DisplayStudy = ({studyData}) => {
-    const [experiments, setExperiments] = useState([]);
-    const [sampleData, setSampleData] = useState([])
-    const accession = studyData.accession;
-    const [experimentWithSamples, setExperimentsWithSamples] = useState({});
+const DisplayStudy = ({ studyData }) => {
+  const [experiments, setExperiments] = useState([]);
+  const [sampleData, setSampleData] = useState([]);
+  const accession = studyData.accession;
+  const study = studyData;
+  const [experimentWithSamples, setExperimentsWithSamples] = useState({});
 
-    //Gets list of all experiments
-    const getExperimentData = async () => {
-      // console.log("in getExperimentData function");
-      try {
-        const response = await fetch(`http://127.0.0.1:2020/get_all_experiments/${accession}`);
-        if (response.status !== 200) {
-          throw new Error("Unable to fetch studies: ", data.message);
-        }
-        const experimentData = await response.json();
-        setExperiments(experimentData.experiments);
-      } catch (error) {
-        console.error("Unable to fetch experiment data: ", error);
-      };
-    };
-
-    //Gets sample data for one experiment
-    const getSampleData = async () => {
-      let experimentWithSamples = {};
-      try {
-          await Promise.all (
-            experiments.map(async (experiment) => {
-              const response = await fetch(`http://127.0.0.1:2020//get_all_samples/${experiment.experiment_id}`);
-              if (response.status !== 200) {
-                throw new Error("Unable to fetch sample data for the experiment: ", response.message);
-              }
-              const samples = await response.json();
-              experimentWithSamples[experiment.experiment_id] = samples;
-              // console.log("sampleData", samples)
-            })
-          );
-          // setExperimentsWithSamples(experimentWithSamples);
-        } catch (error) {
-          console.error("Unable to fetch sample data: ", error);
+  //Gets list of all experiments
+  const getExperimentData = async () => {
+    // console.log("in getExperimentData function");
+    try {
+      const response = await fetch(
+        `http://127.0.0.1:2020/get_all_experiments/${accession}`
+      );
+      if (response.status !== 200) {
+        throw new Error("Unable to fetch studies: ", data.message);
       }
-      console.log("ExeprimentsWithSamples now set", experimentWithSamples);
-      return experimentWithSamples;
-    };
-
-    //Gets all sample data for all experiments.
-    const getAllSampleData = () => {
-        const numberOfExperiments = sampleData.length;
-        // console.log("in getAllSampleData");
-        // console.log(numberOfExperiments);
+      const experimentData = await response.json();
+      setExperiments(experimentData.experiments);
+    } catch (error) {
+      console.error("Unable to fetch experiment data: ", error);
     }
-
-    // ensure experiment data is retrieved first before getting sample data
-    useEffect(() => {
-      getExperimentData();
-    },[]);
-
-    // get sample data for each experiment after experiments are retrieved
-    useEffect(() => {
-      if (experiments.length > 0) {
-        getSampleData().then((experimentWithSamples) => {
-          setExperimentsWithSamples(experimentWithSamples);
-        });
-      }
-    }, [experiments]);
-
-
-    const displayData = () => {
-      const experimentAndSamplesComponents = [];
-      // for (const experiment in experiments) {
-      experiments.forEach (experiment => {
-        // console.log("Experiments", experiments)
-        const expID = experiment.experiment_id;
-        const samples = experimentWithSamples[expID];
-        console.log("Experiment",experiment);
-        console.log("Samples", samples);
-        experimentAndSamplesComponents.push(<DisplayExperimentAndSamples experimentData={experiment} samplesData={samples} />);
-      })
-      console.log("components", experimentAndSamplesComponents);
-      return experimentAndSamplesComponents;
-    }
-
-    return (
-        <div>
-          {displayData().map((form, index) => (
-          <div key={index}>
-            {form}
-          </div>
-          ))}
-        </div>
-  
-  
-    );
   };
+
+  //Gets sample data for one experiment
+  const getSampleData = async () => {
+    let experimentWithSamples = {};
+    try {
+      await Promise.all(
+        experiments.map(async (experiment) => {
+          const response = await fetch(
+            `http://127.0.0.1:2020//get_all_samples/${experiment.experiment_id}`
+          );
+          if (response.status !== 200) {
+            throw new Error(
+              "Unable to fetch sample data for the experiment: ",
+              response.message
+            );
+          }
+          const samples = await response.json();
+          experimentWithSamples[experiment.experiment_id] = samples;
+          // console.log("sampleData", samples)
+        })
+      );
+      // setExperimentsWithSamples(experimentWithSamples);
+    } catch (error) {
+      console.error("Unable to fetch sample data: ", error);
+    }
+    console.log("ExeprimentsWithSamples now set", experimentWithSamples);
+    return experimentWithSamples;
+  };
+
+  //Gets all sample data for all experiments.
+  const getAllSampleData = () => {
+    const numberOfExperiments = sampleData.length;
+    // console.log("in getAllSampleData");
+    // console.log(numberOfExperiments);
+  };
+
+  // ensure experiment data is retrieved first before getting sample data
+  useEffect(() => {
+    getExperimentData();
+  }, []);
+
+  // get sample data for each experiment after experiments are retrieved
+  useEffect(() => {
+    if (experiments.length > 0) {
+      getSampleData().then((experimentWithSamples) => {
+        setExperimentsWithSamples(experimentWithSamples);
+      });
+    }
+  }, [experiments]);
+
+  const displayData = () => {
+    const experimentAndSamplesComponents = [];
+    // for (const experiment in experiments) {
+    experiments.forEach((experiment) => {
+      // console.log("Experiments", experiments)
+      const expID = experiment.experiment_id;
+      const samples = experimentWithSamples[expID];
+      console.log("Experiment", experiment);
+      console.log("Samples", samples);
+      experimentAndSamplesComponents.push(
+        <DisplayExperimentAndSamples
+          experimentData={experiment}
+          samplesData={samples}
+        />
+      );
+    });
+    console.log("components", experimentAndSamplesComponents);
+    return experimentAndSamplesComponents;
+  };
+
+  return (
+    <div>
+      <Card variant="outlined" sx={{ padding: 2 }}>
+        <Grid
+          container
+          rowGap={1}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          spacing={2}
+        >
+          <Grid item sx={{ mt: 1.5 }}>
+            <Typography variant="h4" color="blue-gray" align="center">
+              Study Accession: {study.accession}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            Organism: {study.organism}
+          </Grid>
+          <Grid item xs={6}>
+            Description: {study.description}
+          </Grid>
+          <Grid item xs={6}>
+            Publication: {study.publication}
+          </Grid>
+        </Grid>
+      </Card>
+      {displayData().map((form, index) => (
+        <div key={index}>{form}</div>
+      ))}
+    </div>
+  );
+};
 
 export default DisplayStudy;
