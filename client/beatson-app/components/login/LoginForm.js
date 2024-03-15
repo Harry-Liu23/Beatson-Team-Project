ce7b333aba1ba25d4a9d04011d0f2b7e5dde280b
Aubrey Agub <2555752a@student.gla.ac.uk>
2024-03-15 20:31:43 +0000

    Login  Page Refactored Using MUI
    Necessary to make all components consistent with use of MUI and prevent hardcoding components from scratch.
    Changes include adding the same MUI forms used in upload study forms and increasing spacing as well as simplifying styling.
    Side effects - login button does not work when pressed.

--- client/beatson-app/components/login/LoginForm.js
+++ client/beatson-app/components/login/LoginForm.js
@@ -2,6 +2,16 @@
 import {useState } from 'react';
 import styles from '../../styles/LoginForm.module.css';
 import postFormAsJSON from '../../services/BackendAPI'
+import {
+    Button,
+    Grid,
+    Typography,
+    Card,
+    TextField,
+    spacing,
+    button,
+  } from "@mui/material";
+import LoginIcon from '@mui/icons-material/Login';
 
 /* <LoginForm /> is a component for a form 
 which has a username and password field
@@ -24,36 +34,54 @@ const LoginForm = () => {
     /* saves the user's inputs */
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [institution, setInstitution] = useState("");
 
     return (
-        <div className={styles.container}>
-            <form onSubmit={e => handleSubmit(e)}>
-                <div className={styles.header}> 
-                    Login
-                </div>
-                <br />
-                <div className={styles.inputs}>
-                    <input className={styles.input} 
-                        type="text"
-                        name = "username"
-                        value={username}
-                        placeholder="Enter username"
-                        onChange={(event) => setUsername(event.target.value)}
-                    />
-                    <br /> 
-                    <input className={styles.input} 
-                        type="password"
-                        name = "password"
-                        value={password}
-                        placeholder="Enter password"
-                        onChange={(event) => setPassword(event.target.value)}
-                    />
-                    <br />
-                    <button type="submit" className={styles.button}> 
-                        Login
-                    </button>
-                </div>
-            </form>
+        <div>
+            <Card variant="outlined" sx={{ padding:10 }} > 
+                <Grid
+                    container
+                    rowGap={1}
+                    columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    spacing={2}
+                >
+                    <Grid item xs={6}>
+                        <TextField
+                            id="username"
+                            label="Username"
+                            variant="outlined"
+                            onChange={() => setUsername(event.target.value)}
+                        />
+                    </Grid>
+
+                    <Grid item xs={6}>
+                        <TextField
+                            id="password"
+                            label="Password"
+                            variant="outlined"
+                            onChange={() => setPassword(event.target.value)}
+                        />
+                    </Grid>
+
+                    <Grid item xs={6}>
+                        <Button 
+                            className={styles.button}
+                            color="inherit" 
+                            size="large" 
+                            variant="contained"
+                            startIcon={<LoginIcon/>} 
+                            onClick= {() => {
+                                handleSubmit();
+                            }}>
+                            Login      
+                        </Button> 
+                    </Grid>
+
+                </Grid>
+            </Card>
         </div>
     );
 }
